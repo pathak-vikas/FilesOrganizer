4b13377cbb9426d7e642b59bc52dee7545890ffc
pathak-vikas <pathak.vikas3110@gmail.com>
2021-07-05 01:44:16 +0530

    finalcommit

--- FilesOrganizer/commands/OrganizeRecursive.js
+++ FilesOrganizer/commands/OrganizeRecursive.js
@@ -42,25 +42,26 @@ function OrganizeRecursive_Helper(srcPath) {
     // 3. identify categories of all the files present in that input directory  ->
     let childNames = fs.readdirSync(srcPath);
     let destPath;
-    if (childNames.length == 0) {
-        //if there is no children i.e. no files in current folder--- no need to do anything - 
-        //since if there is no files --> we dont need any sorting as simple as that
-        //simply return on such cases
-        return;
-    }
-    else {
-        //if only folders are present dont do anything
-        for (let i = 0; i < childNames.length; i++) {
-            let childAddress = path.join(srcPath, childNames[i]);
-            let isFile = fs.lstatSync(childAddress).isFile();
-            if (isFile) {
-                // if files are present make one organized folder 
-                destPath = createOrganizedFiles_Folder(srcPath);
-                break;
-            }
-
-        }
-    }
+    /*  if (childNames.length == 0) {
+         //if there is no children i.e. no files in current folder--- no need to do anything - 
+         //since if there is no files --> we dont need any sorting as simple as that
+         //simply return on such cases
+         return;
+     }
+     else {
+         //if only folders are present dont do anything
+         for (let i = 0; i < childNames.length; i++) {
+             let childAddress = path.join(srcPath, childNames[i]);
+             let isFile = fs.lstatSync(childAddress).isFile();
+             if (isFile) {
+                 // if files are present make one organized folder 
+                 destPath = createOrganizedFiles_Folder(srcPath);
+                 break;
+             }
+ 
+         }
+     } */
+    // no need for above checks since our below logic of if file exists make organised folder handles everything  
     // console.log(childNames);
     for (let i = 0; i < childNames.length; i++) {
         let childAddress = path.join(srcPath, childNames[i]);
@@ -69,6 +70,8 @@ function OrganizeRecursive_Helper(srcPath) {
             // console.log(childNames[i]);
             let category = getCategory(childNames[i]);
             //console.log(childNames[i], "belongs to --> ", category);
+            // 2. create -> organized_files_folder -> directory
+            destPath = createOrganizedFiles_Folder(srcPath);
             // 4. copy / cut  files to that organized directory inside of any of category folder 
             sendFiles(childAddress, destPath, category);
         }
